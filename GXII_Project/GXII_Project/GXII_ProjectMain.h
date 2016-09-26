737b3d7f0d9b26b2f61a13c9dddfaa7e55e92a21
Tom Devaney <Glorified Flashlight>
2016-09-26 19:17:52 -0400

    Added project

---
+++ GXII_Project/GXII_Project/GXII_ProjectMain.h
@@ -0,0 +1,35 @@
+﻿#pragma once
+
+#include "Common\StepTimer.h"
+#include "Common\DeviceResources.h"
+#include "Content\Sample3DSceneRenderer.h"
+#include "Content\SampleFpsTextRenderer.h"
+
+// Renders Direct2D and 3D content on the screen.
+namespace GXII_Project
+{
+	class GXII_ProjectMain : public DX::IDeviceNotify
+	{
+	public:
+		GXII_ProjectMain(const std::shared_ptr<DX::DeviceResources>& deviceResources);
+		~GXII_ProjectMain();
+		void CreateWindowSizeDependentResources();
+		void Update();
+		bool Render();
+
+		// IDeviceNotify
+		virtual void OnDeviceLost();
+		virtual void OnDeviceRestored();
+
+	private:
+		// Cached pointer to device resources.
+		std::shared_ptr<DX::DeviceResources> m_deviceResources;
+
+		// TODO: Replace with your own content renderers.
+		std::unique_ptr<Sample3DSceneRenderer> m_sceneRenderer;
+		std::unique_ptr<SampleFpsTextRenderer> m_fpsTextRenderer;
+
+		// Rendering loop timer.
+		DX::StepTimer m_timer;
+	};
+}
